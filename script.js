4608716dc048de0aa4c28d1c2e2fed7e265ea957
ehddus20201 <ehddus20201@naver.com>
2025-06-09 18:40:49 +0900

    0609 수정사항 커밋

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const addInput = document.querySelector('#addInput');
 const addBtn = document.querySelector('#addBtn');
-const allDelBtn = document.querySelector('#allDelBtn')
+const allDelBtn = document.querySelector('#allDelBtn');
 const todoList = document.querySelector('#todoList');
 const inputWrap = todoForm.getElementsByClassName("input_wrap")[0];
 let todoListArray = [];
 
-/* --- 추가 --- */
+let isInitialLoad = true; // 로드 직후
+updateCount(); // isInitialLoad = true 
+
+/* --- 할 일 추가하기 --- */
 
 /* 추가할 li 돔으로 형성 */
 // 보안&유지보수 측면에서는 안전한 방법: dom api로 생성
-function createItem(value, id) {
+function createItem(value, id, isChecked) {
   const li = document.createElement('li');
   li.setAttribute('data-id', id);
-  
+
   const label = document.createElement('label');
   label.className = 'todo_label';
   label.htmlFor = `chk_${id}`;
@@ -21,6 +24,7 @@ function createItem(value, id) {
   chkBox.type = 'checkbox';
   chkBox.className = 'todo_chkbox';
   chkBox.id = `chk_${id}`;
+  chkBox.checked = isChecked;
 
   const chkMark = document.createElement('span');
   chkMark.className = 'chkmark';
@@ -28,7 +32,7 @@ function createItem(value, id) {
   const todoText = document.createElement('span');
   todoText.className = 'todo_text';
   todoText.textContent = value;
-
+  
   // label 안에 요소로 넣음
   // append() : 요소, 텍스트, 여러개 추가가능
   label.append(chkBox, chkMark, todoText);
@@ -36,33 +40,51 @@ function createItem(value, id) {
   const delBtn = document.createElement('button');
   delBtn.type = 'button';
   delBtn.className = 'delete_btn';
-  
+
   const delBlind = document.createElement('span');
   delBlind.className = 'blind';
   delBlind.textContent = '삭제';
-  
+
   //appendChild() : 요소만 추가 가능(텍스트 x), 하나만 추가
   delBtn.appendChild(delBlind);
-  
+
   li.append(label, delBtn);
-  
+
   return li;
 }
 
 /* todoList 안 li 추가 */
-function renderItem({target, value, id}) {
-  const li = createItem(value, id);
+function renderItem({target, value, id, isChecked = false}) {
+  const li = createItem(value, id, isChecked);
   // prepend(): DOM 요소에 자식 요소를 맨 앞에 추가
   target.prepend(li);
 }
 
 /* 추가한 값 배열에 저장 */
-function addItemArray(id, value) {
-
+function addItemArray(id, value, isChecked = false) {
   //unshift() : 맨 앞에 하나 이상의 요소를 추가
-  todoListArray.unshift({ id, value });
+  todoListArray.unshift({ id, value, isChecked });
+  saveTodos();
 }
 
+/* 할일 추가 값이 Null일 경우 경고 표시 */
+function createErrorMsg() {
+    const errorMsg = document.createElement("div");
+    errorMsg.className = "inputErrorMsg";
+    
+    const alertImage = document.createElement("img");
+    alertImage.src = "./assets/images/message-alert-circle.svg"
+    alertImage.alt = "경고"
+    errorMsg.appendChild(alertImage);
+
+    const alertMsg = document.createElement("span");
+    alertMsg.innerText = "할 일을 입력해주세요!"
+    errorMsg.appendChild(alertMsg);
+
+    if (!inputWrap.querySelector(".inputErrorMsg")) {
+    inputWrap.appendChild(errorMsg);
+  } 
+} 
 
 /* 할일 추가 이벤트 */
 function handleTodoList(e) {
@@ -75,34 +97,34 @@ function handleTodoList(e) {
   // trim() :문자열 앞뒤의 공백을 제거
   if (value.trim().length === 0) {
     createErrorMsg();
-    addInput.value = '';
-    
+
   } else {
     renderItem({ target, value, id });
-    addItemArray(id, value);
+    addItemArray(id, value, false);
+    updateCount();
     const existingErrorMsg = inputWrap.querySelector(".inputErrorMsg");
     if (existingErrorMsg) existingErrorMsg.remove();
-    addInput.value = '';
-
   }
+
+  addInput.value = '';
   addInput.focus();
 }
 
 /* 키보드 엔터 이벤트 */
 function handleEnterKey(e) {
-if(e.code === 'Enter' && !e.shiftKey) {
-  handleTodoList();
+  if(e.code === 'Enter' && !e.shiftKey) {
+    //기본동작 차단하기 위해 (줄바꿈, 폼제출 등등)
+    e.preventDefault();
 
-  //기본동작 차단하기 위해 (줄바꿈, 폼제출 등등)
-  e.preventDefault();
-}
+    handleTodoList();
+  }
 }
 
 addInput.addEventListener('keypress', handleEnterKey);
 addBtn.addEventListener('click', handleTodoList);
 
 
-/* --- 삭제 --- */ 
+/* --- 할 일 삭제 --- */
 
 /* 해당 li 돔에서 제거 */
 function removeItem(id) {
@@ -116,6 +138,7 @@ function removeItem(id) {
 function removeItemArray(id) {
   //filter() : 배열을 순회하며 콜백함수 조건을 만족하는 요소만 모아 새배열 생성
   todoListArray = todoListArray.filter(todo => todo.id !== id);
+  saveTodos();
 }
 
 /* 삭제 버튼 이벤트 */
@@ -131,16 +154,9 @@ function handleRemove(e) {
 
   removeItem(id);
   removeItemArray(id);
+  updateCount();
 }
 
-/* 근접한 delete_btn 찾아서 클릭 이벤트 */
-function handleDelClick(e) {
-  if (e.target.closest('.delete_btn')) {
-    handleRemove(e);
-  }
-}
-
-todoList.addEventListener('click', handleDelClick);
 
 /* 전체 삭제 */
 function removeAllItem(){
@@ -152,46 +168,107 @@ function removeAllItem(){
 
 function removeAllItemArray(){
   todoListArray = [];
-  return todoListArray;
-
+  saveTodos();
 }
 
 function handleRemoveAll(e) {
   const li = todoList.querySelector('li');
-  const confirmAns = confirm('할 일 목록을 전부 삭제하시겠습니까?');
-  if(!li) alert('삭제할 항목이 없습니다.');
+  if(!li){
+    alert('삭제할 항목이 없습니다.');
+    return;
+  }
 
-  else if(confirmAns){ 
+  const confirmAns = confirm('할 일 목록을 전부 삭제하시겠습니까?');
+  if(confirmAns){ 
     removeAllItem();
     removeAllItemArray();
+    isInitialLoad = true;
+    updateCount();
   }
 }
 
 allDelBtn.addEventListener('click',handleRemoveAll);
 // 삭제 버튼은 동적으로 생성되기 때문에 부모에 이벤트
-todoList.addEventListener('click',handleDelClick);
+todoList.addEventListener('click',handleRemove);
 
 
-function createErrorMsg() {
-    const errorMsg = document.createElement("div");
-    errorMsg.className = "inputErrorMsg";
-    errorMsg.innerHTML = `
-        <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M11.9991 15.375V12M11.9991 8.625V8.70959M20.9983 12C20.9983 13.2938 20.7253 14.5238 20.2338 15.6356L21 20.9991L16.4039 19.85C15.1019 20.5823 13.5993 21 11.9991 21C7.02906 21 3 16.9706 3 12C3 7.02944 7.02906 3 11.9991 3C16.9692 3 20.9983 7.02944 20.9983 12Z"
-                stroke="#FF6174"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round" />
-		</svg>
-		<span>할 일을 입력해주세요!</span>`
 
-    if (!inputWrap.querySelector(".inputErrorMsg")) {
-    inputWrap.appendChild(errorMsg);
-  } 
-} 
+/* --- 해야할 일 갯수 표시 --- */
+
+// 할 일 개수 표시를 업데이트
+function updateCount(){
+    const countWrap = document.querySelector("#todoCount");
+    if(!countWrap) return;
+
+    
+    let count = 0;
+    todoListArray.forEach(todo => {
+    const checkbox = document.querySelector(`#chk_${todo.id}`);
+    if (checkbox && !checkbox.checked) {
+      count++;
+    }});
+
+    if(todoListArray.length === 0 && isInitialLoad){
+        countWrap.innerHTML = "오늘 할 일을 적어봐 !! 🤓";
+        return;
+      }
+    
+    if(todoListArray.length === 0 || count === 0) {
+        countWrap.innerHTML =  "오늘 할 일 끝 !! 🥳";
+    } else {
+      countWrap.innerHTML = `남은 할 일 : <span id="remainingCount">${count}</span>개`;
+    };
+
+    isInitialLoad = false; 
+}
+
+
+// 체크박스가 바뀔 때 실행
+function handleCheckbox(e) {
+    if(e.target.classList.contains("todo_chkbox")){
+        updateCount();
+        changeArray(e);
+    }
+}
+
+todoList.addEventListener("change", handleCheckbox);
+
+
+/* 체크할 때마다 배열 상태 변경 및 저장 */
+function changeArray(e) {
+  if (!e.target.matches('input[type="checkbox"]')) return;
+
+  const li = e.target.closest('li');
+  const id = li.dataset.id;
+  const isChecked = e.target.checked;
+
+  const todo = todoListArray.find(todo => todo.id === id);
+  if (todo) {
+    todo.isChecked = isChecked;
+    saveTodos();
+  }
+};
+
+
+/* localStorage 저장 */
+const TODOS_KEY = "todoArray"
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todoListArray));
+}
+
+/* localStorage에서 불러오기 */
+const savedTodoList = localStorage.getItem(TODOS_KEY);
+if (savedTodoList) {
+  const parsedTodoList = JSON.parse(savedTodoList);
+
+  parsedTodoList.forEach( function(item){
+  const li = createItem(item.value, item.id, item.isChecked);
+  todoList.append(li); 
+
+  // todoListArray에 localStorage 저장 항목 추가
+  todoListArray = parsedTodoList.slice();
+  isInitialLoad = false;
+  updateCount();
+  } );
+
+}
